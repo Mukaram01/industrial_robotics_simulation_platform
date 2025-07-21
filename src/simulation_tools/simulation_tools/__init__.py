61b7562706e9689c4165fa3a0c835dc68b50cb61
notanexpert <121873648+Mukaram01@users.noreply.github.com>
2025-07-21 16:19:14 +0100

    Add joint plotter node and launch option

--- src/simulation_tools/simulation_tools/__init__.py
+++ src/simulation_tools/simulation_tools/__init__.py
@@ -1,5 +1,9 @@
 """Utility nodes for simulation tools."""
 
-__all__ = ['Object3DViewerNode']
+__all__ = [
+    'Object3DViewerNode',
+    'JointPlotterNode',
+]
 
 from .object_3d_viewer_node import Object3DViewerNode
+from .joint_plotter_node import JointPlotterNode
